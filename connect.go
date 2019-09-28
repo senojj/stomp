989c559e1938e71843cb468507a0b600fc6fd31b
Joshua Jones <joshua.jones.software@gmail.com>
2019-09-28 01:45:39 -0400

    begin work on connecting

--- connect.go
+++ connect.go
@@ -2,6 +2,7 @@ package stomp
 
 import (
 	"github.com/dynata/stomp/proto"
+	"net"
 )
 
 type Connection struct {
@@ -10,16 +11,32 @@ type Connection struct {
 	server  string
 }
 
-type ConnOpt func(map[string]string)
+type Option proto.Header
 
-func WithCredentials(login, passcode string) ConnOpt {
-	return func(m map[string]string) {
-		m[proto.HdrLogin] = login
-		m[proto.HdrPasscode] = passcode
+func (o Option) Set(name string, value ...string) {
+	o[name] = value
+}
+
+func WithCredentials(login, passcode string) func(Option) {
+	return func(option Option) {
+		option.Set(proto.HdrLogin, login)
+		option.Set(proto.HdrPasscode, passcode)
 	}
 }
-/*
-func Connect(c net.Conn, options ...ConnOpt) Connection {
 
+func Connect(c net.Conn, options ...func(Option)) (*Connection, error) {
+	host, _, splitErr := net.SplitHostPort(c.RemoteAddr().String())
+
+	if nil != splitErr {
+		return nil, splitErr
+	}
+	frame := proto.NewFrame(proto.CmdConnect, nil)
+
+	frame.Header.Set(proto.HdrHost, host)
+
+	for _, option := range options {
+		option(Option(frame.Header))
+	}
+
+	return nil, nil
 }
-*/
