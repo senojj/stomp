1e9c6f5094cb35210e5a1425c60cedb82d1f51c3
Joshua Jones <joshua.jones.software@gmail.com>
2019-09-29 21:27:48 -0400

    a bit of progress

--- connect.go
+++ connect.go
@@ -33,10 +33,33 @@ func Connect(c net.Conn, options ...func(Option)) (*Connection, error) {
 	frame := proto.NewFrame(proto.CmdConnect, nil)
 
 	frame.Header.Set(proto.HdrHost, host)
+	frame.Header.Set(proto.HdrAcceptVersion, "1.0,1.1")
 
 	for _, option := range options {
 		option(Option(frame.Header))
 	}
+	_, frameWrtErr := frame.WriteTo(c)
 
+	if nil != frameWrtErr {
+		return nil, frameWrtErr
+	}
+	frameReader := proto.NewFrameReader(c)
+
+	respFrame, frameRdErr := frameReader.Read()
+
+	if nil != frameRdErr {
+		return nil, frameRdErr
+	}
+
+	if respFrame.Command == proto.CmdError {
+		// TODO: handle error
+		return nil, nil
+	}
+
+	if respFrame.Command != proto.CmdConnected {
+		// TODO: return error
+		return nil, nil
+	}
+	// TODO: handle Connected frame
 	return nil, nil
 }
