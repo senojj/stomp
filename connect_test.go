e1ae068f797f8cc9b88174165283d5f6374aabe9
Joshua Jones <joshua.jones@dynata.com>
2019-10-09 15:25:02 -0400

    modified test

--- connect_test.go
+++ connect_test.go
@@ -146,15 +146,16 @@ func TestSession_Subscribe(t *testing.T) {
 		if nil != closeErr {
 			t.Fatal(closeErr)
 		}
-		fmt.Printf("received message - %s\n", string(body))
+		t.Logf("received message - %s\n", string(body))
 	})
 	cancel()
 
 	if nil != subErr {
 		t.Fatal(subErr)
 	}
+	t.Logf("successfully subscribed...")
 	time.Sleep(5*time.Second)
-	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
 	unsubErr := subscription.Unsubscribe(ctx, WithReceipt("1AB2C3"))
 	cancel()
 
@@ -164,6 +165,6 @@ func TestSession_Subscribe(t *testing.T) {
 	clsErr := session.Close()
 
 	if nil != clsErr {
-		fmt.Println(clsErr)
+		t.Fatal(clsErr)
 	}
 }
